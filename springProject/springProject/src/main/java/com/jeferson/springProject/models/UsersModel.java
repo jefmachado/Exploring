fd45e372574ff7788cec41ae78eb2c26e5b57010
Jeferson Machado <jeferson.4machado@gmail.com>
2024-02-12 17:00:25 -0300

    Novo Projeto SpringBoot

---
+++ springProject/springProject/src/main/java/com/jeferson/springProject/models/UsersModel.java
@@ -0,0 +1,27 @@
+package com.jeferson.springProject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class UsersModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String email;
+    private String CPF;
+    private Integer telefone;
+
+}
